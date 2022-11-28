1c47f782fdafc35c91ee93846ae2ca6cd8c6e940
zimbajose <61836582+zimbajose@users.noreply.github.com>
2022-11-28 15:57:01 -0300

    Atualiza repositorio

--- bin/main.js
+++ bin/main.js
@@ -1,16 +1,95 @@
+
 const loopTime = 0.12; // Tempo de loop de jogo
 
 var racetime = 0; //Tempo em milisegundos da corrida
 
+var listener = new window.keypress.Listener() //Leitor de teclas
+var keyScope = this;
 
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(35,window.innerWidth/window.innerHeight);
+var render = new THREE.WebGLRenderer({antialiasing:true});
+camera.position.z = 500;
+var chassiGeometry = new THREE.BoxGeometry(4, 2, 0.7);
+var carTexture = new THREE.MeshBasicMaterial({ color: 0xffaa10 }); //Chassi
+var chassi = new THREE.Mesh(chassiGeometry, carTexture);
+var chassi2 = new THREE.Mesh(new THREE.BoxGeometry(3, 1, 0.7), carTexture);
+chassi2.position.x = 2;
+render.setSize(window.innerWidth, window.innerHeight);
+var canvas = render.domElement;
+var car = new THREE.Group();
+car.add(chassi);
+car.add(chassi2);
+
+scene.add(car);
+
+const light = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(light);
+document.body.appendChild(canvas);
 
 function renderLoop() {
+    render.render(scene,camera)
     requestAnimationFrame(renderLoop);
 }
 requestAnimationFrame(renderLoop);
 
-async function gameLoop() {
+var phyi = new PhysicsInstance(1,0.001,0.1,0.004,0.005,car);
+//Eventos de teclado
+var keyboard_events = listener.register_many([
+    {
+        "keys" : "w",
+        "is_exclusive" : true,
+        //Acelera o carro
+        "on_keydown": function(){
+            phyi.accelerating = true;
+        },
+        "on_keyup" : function(){
+            phyi.accelerating = false;
+        },
+        "this": keyScope
+    },
+    {
+        "keys" : "s",
+        "is_exclusive" : true,
+        //Acelera o carro
+        "on_keydown": function(){
+            phyi.braking = true;
+        },
+        "on_keyup" : function(){
+            phyi.braking = false;
+        },
+        "this": keyScope
+    },
+    {
+        "keys" : "a",
+        "is_exclusive" : true,
+        //Acelera o carro
+        "on_keydown": function(){
+            phyi.leftTurning = true;
+        },
+        "on_keyup" : function(){
+            phyi.leftTurning = false;
+        },
+        "this": keyScope
+    },
+    {
+        "keys" : "d",
+        "is_exclusive" : true,
+        //Acelera o carro
+        "on_keydown": function(){
+            phyi.rightTurning = true;
+        },
+        "on_keyup" : function(){
+            phyi.rightTurning = false;
+        },
+        "this": keyScope
+    }
+]);
 
+
+async function gameLoop() {
+    phyi.calculateNextPosition();
 }
 
 setInterval(gameLoop, loopTime);
+
--- bin/physicsinstance.js
+++ bin/physicsinstance.js
@@ -1,15 +1,13 @@
 //Uma instancia de física para um veiculo do jogo
 class PhysicsInstance {
 
+    
 
-
-    constructor(traction, acceleration, maxspeed, breakPower, aero, position = new THREE.Vector3(1, 0, 0), direction) {
+    constructor(traction, acceleration, maxspeed, breakPower, aero, vehicleModel) {
         this.traction = traction;
         this.acceleration = acceleration;
         this.maxspeed = maxspeed;
-
-        //Posição do carro se não definida é tomada como 0
-        this.position = position;
+        this.vehicleModel = vehicleModel;
         //Rotação do veiculo possui 16 direções com 1 tomando 
         this.rotation = 0;
 
@@ -18,60 +16,146 @@ class PhysicsInstance {
         this.aero = aero;
         //Velocidade do carro
         this.speed = new THREE.Vector3(0, 0, 0);
-
         //Deltas de aceleração do carro
         this.deltas = new THREE.Vector3(0, 0, 0);
-        this.direction = new THREE.Vector3(1, 0, 0);
+        //Controles
+        this.accelerating = false;
+        this.leftTurning = false;
+        this.rightTurning = false;
+        this.braking = false;
+        
+        //Contador de curva, a cada 80 frames ele permite o carro virar mais uma vez.
+        this.turnCounter = 80;
+        this.vehicleModel.rotations = [
+            0,
+            Math.PI/8,
+            Math.PI/4,
+            (Math.PI/180) * 67.5,
+            Math.PI/2,
+            (Math.PI/180)*112.5,
+            (Math.PI/180)*135,
+            (Math.PI/180)*157.5,
+            Math.PI,
+            (Math.PI/180)*202.5,
+            (Math.PI/180)*225,
+            (Math.PI/180)*247.5,
+            (Math.PI/180)*270,
+            (Math.PI/180)*290.5,
+            (Math.PI/180)*315,
+            (Math.PI/180)*337.5,
+        ]
     }
 
 
     //Acelera o carro
     accelerate() {
-            let angle = getAngle(this.rotation)
-
-            let deltax = (Math.cos(angle) * (this.speed.x + this.acceleration));
-            let deltay = (Math.sin(angle) * (this.speed.y + this.acceleration));
-            this.deltas.x = deltax;
-            this.deltas.y = deltay;
-        }
-        //Freia o carro
+        //Calcula deltas de aceleração
+        let deltax = (Math.cos(this.vehicleModel.rotation.z) * (this.acceleration));
+        let deltay = (Math.sin(this.vehicleModel.rotation.z) * (this.acceleration));
+        //console.log(this.speed);
+        this.speed.x+= deltax;
+        this.speed.y +=deltay;
+        //console.log(this.speed);
+            
+    }
+    calculateDeltas(){
+        let truespeed = this.speed.length();
+        let deltax = (Math.cos(this.vehicleModel.rotation.z) * (truespeed));
+        let deltay = (Math.sin(this.vehicleModel.rotation.z) * (truespeed));
+        this.deltas.x = deltax;
+        this.deltas.y = deltay;
+    }
+    
+    //Freia o carro
     break () {
-        let angle = getAngle(this.rotation)
+        //Calcula deltas de desaceleração
+        let deltax = (Math.cos(this.vehicleModel.rotation.z) * (this.breakPower));
+        let deltay = (Math.sin(this.vehicleModel.rotation.z) * (this.breakPower));
+        this.speed.x-= deltax;
+        this.speed.y-=deltay;
+        //Os leva para 0 caso o carro freie demais
+        if(Math.abs(this.speed.length())<=this.breakPower*5){
+            this.speed.x =0;
+            this.speed.y =0;
+        }
+    }
+    //Reduz a velocidade por conta do empuxo do ar do carro
+    reduceByAero(){
+       
+        let aeroReduction = this.aero*Math.pow(this.speed.length(),2);
+        
+       
+        let deltax = (Math.cos(this.vehicleModel.rotation.z) * (aeroReduction));
+        let deltay = (Math.sin(this.vehicleModel.rotation.z) * (aeroReduction));
+       
+        this.speed.x-= deltax;
+        this.speed.y-=deltay;
 
-        let deltax = (Math.cos(angle) * this.breakPower);
-        let deltay = (Math.sin(angle) * this.breakPower);
-        this.deltas.x = -deltax;
-        this.deltas.y = -deltay;
+        /*
+        this.speed.x-= deltax;
+        this.speed.y-=deltay;
+        //Arredonda pra 0 quando o carro está perto de parar
+        if(Math.abs(this.speed.length())<=aeroReduction*2){
+            this.speed.x =0;
+            this.speed.y =0;
+        }*/
     }
 
     //Calcula novas velocidades x e y
     calculateNewSpeed() {
+        //Reduz o contador de virar o carro
+        this.turnCounter = this.turnCounter>0? this.turnCounter-1 : 0;
+        //Mudança de direção
+        if(this.turnCounter==0 && Math.abs(this.speed.length())>=this.acceleration*80){
+            if(this.rightTurning){
+                this.turnCounter = 80;
+                this.turnCar(true);
+            }
+            else if(this.leftTurning){
+            this.turnCounter = 80;
+            this.turnCar(false);
+            }  
+        }
+        
         //Calcula aceleração e freio
-
-        //Calcula mudança de direção
-
+        if(this.braking){
+            this.break();
+            console.log("yes");
+        }
+        else if(this.accelerating){
+            this.accelerate();
+        }
+        
         //Calcula arrasto do ar
-
+        this.reduceByAero();
         //Altera velocidade
+        this.calculateDeltas();//Calcula novos deltas
+       
         this.speed.multiplyScalar(100 - this.traction);
-        this.speed.addScalar(this.speed, this.deltas);
+        this.speed.addVectors(this.speed, this.deltas);
         this.speed.divideScalar(100 - this.traction + 1);
-        this.position.add(this.speed);
+       
 
     }
 
     //Atualiza as posições
-    calculateNextPos() {
-            this.position.x += this.speed.x;
-            this.position.y += this.speed.y;
-            this.position.z += this.speed.z;
+    calculateNextPosition() {
+            this.calculateNewSpeed();
+            this.vehicleModel.position.add(this.speed);
         }
         //True para direita false para esquerda
-    turnCar(direction = true) {
-        if (direction) {
-            this.rotation = this.rotation == 16 ? 1 : this.rotation + 1;
+    turnCar(direction) {
+        if (!direction) {
+            this.rotation = this.rotation == 15 ? 0 : this.rotation + 1;
+            //this.vehicleModel.rotation.z = this.getAngle(this.rotation);
+            this.vehicleModel.rotation.z = this.vehicleModel.rotations[this.rotation];
+            //this.vehicleModel.rotation.z+=(360/17);
         } else {
-            this.rotation = this.rotation == 1 ? 16 : this.rotation - 1;
+            this.rotation = this.rotation == 0 ? 15 : this.rotation - 1;
+            //this.vehicleModel.rotation.z = this.getAngle(this.rotation);
+            this.vehicleModel.rotation.z = this.vehicleModel.rotations[this.rotation];
+            
+            //this.vehicleModel.rotation.z-=(360/17);
         }
     }
 
@@ -86,8 +170,8 @@ class PhysicsInstance {
 
 
     //Converte um número das 16 direções para um angulo
-    static getAngle(rotation) {
-        return rotation * (360 / 17);
+    getAngle(rotation) {
+        return rotation * (180 / 16);
     }
 
 
